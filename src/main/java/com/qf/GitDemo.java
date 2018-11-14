c44aff4e591b37b1186df4f4ceaf8e031c7f3988
pyn <1786760793@qq.com>
2018-11-14 21:08:52 +0800

    第一次架构搭建完成

---
+++ src/main/java/com/qf/GitDemo.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @Author pyn
+ * @Time 2018/11/14 21:00
+ * @Version 1.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+    }
+}
